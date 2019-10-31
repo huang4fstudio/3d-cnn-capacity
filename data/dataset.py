f741a5986d3916279a5ed2cad52ac667326f151f
DavidMChan <david@iridescent.io>
2019-10-31 15:53:49 -0700

    Added UCF101

--- data/dataset.py
+++ data/dataset.py
@@ -1,11 +1,13 @@
 
+import os
 import json
+
 import torch
 import numpy as np
 
 
-class Kinetics400(torch.utils.data.Dataset):
-    def __init__(self, dataset_json):
+class ActivityRecognitionDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_json, dataset_root):
         # Load the dataset
         with open(dataset_json, 'r') as jf:
             self.dataset_map = json.load(jf)
@@ -16,6 +18,6 @@ class Kinetics400(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         elem = self.dataset_map[idx]
         return {
-            'video': np.load(elem['video_path']),
+            'video': np.load(os.path.join(dataset_root, elem['video_path'])),
             'class': elem['class'],
         }
---
+++ data/ucf101/preprocess_dataset.py
@@ -0,0 +1,70 @@
+import os
+import random
+import glob
+
+import tqdm
+import numpy as np
+import multiprocessing.pool
+
+from alexandria.util import write_json, load_json
+from alexandria.util.video import load_video_with_fps, downsample_video_fps, resize_video
+
+# Open the dataset
+dataset_root_path = '/data/vision/ucf101/UCF101/'
+output_root_path = '/data/vision/ucf101/downsampled/'
+
+def proc_sample(sample):
+
+    idx, s = sample
+
+    # Subsample and preprocess the video
+    try:
+        data = load_video_with_fps(s['path'])
+        if data['frames'] is not None and data['frames'].shape[0] > 0:
+            downsampled = downsample_video_fps(data['frames'], data['fps'] if data['fps'] else 30, 5)
+            resized = resize_video(downsampled, (224,224))
+
+            # Write the file
+            np.save(os.path.join(output_root_path, '{}.npy'.format(idx)), resized)
+
+            output = {
+                'path': '{}.npy'.format(idx),
+                'video_path': s['path'],
+                'id': idx,
+                'class': s['class'],
+                'class_raw': s['class_raw'],
+                'split': s['split'],
+            }
+
+            return output
+        return None
+    except Exception as ex:
+        print(s, ex)
+        return None
+
+
+# Discover samples for the dataset
+files = glob.glob(dataset_root_path + '*.avi')
+if not os.path.exists('class_map.json'):
+    classes = set([f.split('_')[1] for f in files])
+    class_map = {c:idx for idx, c in enumerate(classes)}
+    write_json(class_map, 'data/ucf101/class_map.json')
+else:
+    class_map = load_json('data/ucf101/class_map.json')
+
+dataset = [{
+    'path': f,
+    'class': class_map[f.split('_')[1]],
+    'class_raw': f.split('_')[1],
+    'split': 'train' if random.random() < 0.9 else 'val',
+} for f in files]
+
+elems = []
+with multiprocessing.pool.Pool(6) as pool:
+    for e in tqdm.tqdm(pool.imap_unordered(proc_sample, enumerate(dataset)), total=len(dataset)):
+        if e is not None:
+            elems.append(e)
+
+
+write_json([e for e in elems if e['split'] == 'train'], 'data/ucf101/ucf101_train.json')
+write_json([e for e in elems if e['split'] == 'val'], 'data/ucf101/ucf101_val.json')

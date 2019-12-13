c16045764c14254dc14989a6d05104d1fe6caf81
Forrest Huang <huang.4fstudio@gmail.com>
2019-12-12 18:02:02 -0800

    sklearn

--- nearest_neighbour.py
+++ nearest_neighbour.py
@@ -3,6 +3,7 @@ import numpy as np
 import click
 import tqdm
 from data.dataset import ActivityRecognitionDataset
+from sklearn.neighbors import NearestNeighbors
 
 def main():
     batch_size = 1
@@ -10,28 +11,24 @@ def main():
     data_loader_1 = torch.utils.data.DataLoader(dataset, batch_size=batch_size, pin_memory=True, num_workers=5)
     data_loader_2 = torch.utils.data.DataLoader(dataset, batch_size=batch_size, pin_memory=True, num_workers=5)
 
-    correct_cls = 0
     data_cls = 0
+    all_pts = []
+    all_l = []
     for e in tqdm.tqdm(data_loader_1):
         data_1 = e['video']
         labels = e['class']
-        min_dist = 1e10
-        class_out = -1
         mean_frame_1 = data_1.mean(dim=1)
-        for e in data_loader_2:
-            data_2 = e['video']
-            labels_2 = e['class']
-
-            mean_frame_2 = data_2.mean(dim=1)
-            dist = ((mean_frame_1 - mean_frame_2) ** 2).sum()
-            if dist.item() < min_dist:
-                class_out = labels_2[0]
-                min_dist = dist.item()
-        
-        if class_out.item() == labels[0].item():
-            correct_cls += 1
+        all_pts.append(mean_frame_1.item())
+        all_l.append(labels[0].item())
         data_cls += 1
+    
+    all_pts = np.array(all_pts)
+    all_l = np.array(all_l)
+    
+    nbrs = NearestNeighbors(n_neighbors=2).fit(all_pts)
+    _, indices = nbrs.kneighbors(all_pts)
 
+    correct_cls = sum(all_l[indices[:, 1]] == all_l[indices[:, 0]])
                     
     print('Done!')
     print('Acc: {}'.format(correct_cls/data_cls))
